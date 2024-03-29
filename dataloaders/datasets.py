fixed ball coordinate scaling, added ball detector

dataloaders/datasets.py
@@ -10,7 +10,12 @@ from dataloaders.heatmap import generate_heatmap
 
 
 class TrackNetDataset(Dataset):
-    """Dataset for TrackNet model."""
+    """
+    Dataset for TrackNet model.
+    
+    Assumes ball tracking annotations are in file called Label.csv with image names in column 0,
+    ball x-coordinate in column 2, and ball y-coordinate in column 3.
+    """
     def __init__(
             self,
             path: str,
@@ -23,16 +28,16 @@ class TrackNetDataset(Dataset):
         TrackNet dataset initializer.
         
         Args:
-            path (str): absolute path to dataset
-            shape (Tuple[int, int]): output image shape as height, width
-            input_size (int): number of input images for model
-            output_size (int): number of outputs for model
-            mode (ImageReadMode): torch image reading mode
-            has_labels (bool): whether or not dataset has labels
+            path (str): Absolute path to dataset.
+            shape (Tuple[int, int]): Output image shape as height, width.
+            input_size (int): Number of input images for model.
+            output_size (int): Number of outputs for model.
+            mode (ImageReadMode): Torch image reading mode.
+            has_labels (bool): Whether or not the dataset has labels.
         """
         if input_size < output_size:
             raise ValueError('Cannot have more outputs than inputs')
-
+        
         self._path = path
         self._annotations = pd.read_csv(f'{path}/Label.csv').fillna(-1)
         self._shape = shape
@@ -45,6 +50,18 @@ class TrackNetDataset(Dataset):
         self._mode = mode
         self._has_labels = has_labels
 
+    @property
+    def shape(self):
+        """Output image shape."""
+        return self._shape
+
+    @property
+    def summary(self):
+        """Dataset summary."""
+        return (f'Image Output Shape: {self._shape}, '
+                f'Number of Images: {self._input_size}, '
+                f'Number of Outputs: {self._output_size}')
+
     def __len__(self):
         """Dataset length."""
         return len(self._annotations) - self._input_size
@@ -54,30 +71,37 @@ class TrackNetDataset(Dataset):
         Dataset indexer.
 
         Args:
-            index (Union[int, torch.Tensor]): index
+            index (Union[int, torch.Tensor]): Index.
         """
         if torch.is_tensor(index): index = index.tolist()
 
         image_names = [self._annotations.iloc[index+i, 0] for i in range(self._input_size)]
         images = [read_image(f'{self._path}/{image_name}', self._mode) for image_name in image_names]
-        images = [self._transform(image) for image in images]
-        images = torch.cat(images, dim=0)
+        
+        raw_shape = (images[0].size(dim=1), images[0].size(dim=2))
+
+        images = torch.cat([self._transform(image) for image in images], dim=0)
 
         if self._has_labels:
+            h_ratio, w_ratio = self._shape[0] / raw_shape[0], self._shape[1] / raw_shape[1]
             start = index + self._input_size - self._output_size
             centers = [
-                (self._annotations.iloc[start+i, 1], self._annotations.iloc[start+i, 2])
+                (int(h_ratio * self._annotations.iloc[start+i, 2]),
+                 int(w_ratio * self._annotations.iloc[start+i, 3]))
                 for i in range(self._output_size)
             ]
-            labels = [torch.from_numpy(generate_heatmap(self._shape, center)) for center in centers]
-            labels = torch.stack(labels, dim=0)
+            labels = torch.stack(
+                [torch.from_numpy(generate_heatmap(self._shape, center)) for center in centers], dim=0
+            )
             return images, labels
         return images
 
 
 if __name__ == '__main__':
+    # testing the TrackNetDataset class
     dirname = os.path.dirname(__file__)
     path = os.path.join(dirname, '../data/game1/Clip1')
     dataset = TrackNetDataset(path, shape=(288, 512))
     print('Sample Input Shape:', dataset[0][0].size())
     print('Sample Label Shape:', dataset[0][1].size())
+    print(dataset.summary)

dataloaders/heatmap.py
@@ -18,10 +18,10 @@ def generate_heatmap(
     Generate binary heatmap.
     
     Args:
-        shape (Tuple[int, int]): output heatmap shape as height, width
-        center (Tuple[int, int]): ball center in image coordinates as x, y (zero-indexed)
-        radius (int): radius of generated circle
-        type (np.dtype): heatmap datatype (defaults to np.float32)
+        shape (Tuple[int, int]): Output heatmap shape as height, width.
+        center (Tuple[int, int]): Ball center in image coordinates as x, y (zero-indexed).
+        radius (int): Radius of generated circle.
+        type (np.dtype): Heatmap datatype (defaults to np.float32).
 
     Returns:
         Binary heatmap for ball.
@@ -39,4 +39,5 @@ def generate_heatmap(
 
 
 if __name__ == '__main__':
+    # testing the generate_heatmap function
     print(generate_heatmap(shape=(5, 7), center=(3, 2)))

losses/focal_loss.py
@@ -15,7 +15,7 @@ class BinaryFocalLoss(nn.Module):
         Binary focal loss initializer.
 
         Args:
-            gamma (float): gamma coefficient for focal loss
+            gamma (float): Gamma coefficient for focal loss.
         """
         super().__init__()
         self._gamma = gamma
@@ -31,6 +31,7 @@ class BinaryFocalLoss(nn.Module):
         Returns:
             Binary focal loss.
         """
-        loss = targets * (1 - inputs)**self._gamma * torch.log(inputs) + (1 - targets) * inputs**self._gamma * torch.log(1 - inputs)
+        loss = (targets * (1 - inputs)**self._gamma * torch.log(inputs) +
+                (1 - targets) * inputs**self._gamma * torch.log(1 - inputs))
         loss = torch.mean(-loss)
         return loss

main.py
@@ -1,26 +1,29 @@
 """Main script."""
 import os
 from tqdm import tqdm
+import numpy as np
 import torch
 from torch.utils.data import DataLoader
 from torch.optim import Adam
-from losses.focal_loss import BinaryFocalLoss
 from torchvision.io import ImageReadMode
 
 from dataloaders.datasets import TrackNetDataset
 from models.tracknet import TrackNet
+from losses.focal_loss import BinaryFocalLoss
 
 
 # TODO: Move training to a trainer class
 if __name__ == '__main__':
+    dirname = os.path.dirname(__file__)
+
     input_size = 3
     output_size = 1
 
     model = TrackNet(input_size, output_size)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
+    # model.load_state_dict(torch.load(os.path.join(dirname, './weights/small_tracknet.pt'), map_location=torch.device(device)))
     model.to(device)
 
-    dirname = os.path.dirname(__file__)
     path = os.path.join(dirname, './data/game1/Clip1')
     dataset = TrackNetDataset(
         path,
@@ -29,7 +32,7 @@ if __name__ == '__main__':
         output_size=output_size,
         mode=ImageReadMode.GRAY
     )
-    dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
 
     loss_criterion = BinaryFocalLoss()
     optimizer = Adam(model.parameters(), lr=1e-3)
@@ -55,3 +58,8 @@ if __name__ == '__main__':
     print(train_loss)
 
     torch.save(model.state_dict(), os.path.join(dirname, './weights/small_tracknet.pt'))
+
+    # model.eval()
+    # # pred = model(dataset[0][0])
+    # print(np.where(dataset[0][1].numpy() == 1))
+    # print(model.detect_ball(dataset[0][1]))

models/tracknet.py
@@ -7,6 +7,8 @@ Attention architecture from https://arxiv.org/abs/1804.03999.
 Res-UNet architecture from https://github.com/Chang-Chia-Chi/TrackNet-Badminton-Tracking-tensorflow2.
 """
 from typing import List
+import numpy as np
+import cv2
 import torch
 import torch.nn as nn
 
@@ -23,6 +25,8 @@ class TrackNet(nn.Module):
         https://github.com/nttcom/WASB-SBDT
         https://github.com/sfczekalski/attention_unet
     """
+    _score_threshold = 0.5
+
     def __init__(
             self,
             in_channels: int,
@@ -42,16 +46,14 @@ class TrackNet(nn.Module):
         """
         super().__init__()
 
-        if len(blocks) < 4 or len(channels) < 4:
-            raise ValueError('There must be at least 4 blocks and 4 channels')
+        if len(blocks) != 4 or len(channels) != 4:
+            raise ValueError('There must be 4 blocks and 4 channels')
 
         self._conv_in = InputConvBlock(in_channels, neck_channels)
         self._down1 = DownStack(neck_channels, channels[0], blocks[0])
         self._down2 = DownStack(channels[0], channels[1], blocks[1])
         self._down3 = DownStack(channels[1], channels[2], blocks[2])
         self._down4 = DownStack(channels[2], channels[3], blocks[3])
-
-        
 
         self._up1 = UpStack(channels[3], channels[2], channels[2], blocks[2])
         self._up2 = UpStack(channels[2], channels[1], channels[1], blocks[1])
@@ -81,3 +83,24 @@ class TrackNet(nn.Module):
         x9 = self._up4(x8)
         out = self._conv_out(x9)
         return out
+    
+    @classmethod
+    def detect_ball(cls, heatmap: torch.Tensor):
+        """
+        Detect ball in heatmap.
+
+        Args:
+            heatmap (torch.Tensor): Raw heatmap.
+        
+        Returns:
+            Ball center coordinates.
+        """
+        heatmap = np.squeeze(heatmap.numpy())
+        if np.max(heatmap) < cls._score_threshold: return (-1, -1)
+
+        _, binary_map = cv2.threshold(heatmap, cls._score_threshold, 1, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(binary_map.astype('uint8'), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contour = max(contours, key=cv2.contourArea)
+        moment = cv2.moments(contour)
+        center = (int(moment['m10'] // moment['m00']), int(moment['m01'] // moment['m00']))
+        return center

models/tracknet_modules.py
@@ -75,7 +75,7 @@ class ResBlock(nn.Module):
         """
         super().__init__()
 
-        self._short_cut = lambda x: x
+        self._short_cut = nn.Identity()
         conv2 = nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1)
         if type is ResBlockType.DOWNSAMPLER:
             self._short_cut = nn.Sequential(
@@ -127,10 +127,6 @@ class ResBlock(nn.Module):
         x1 = self._conv1(x)
         x2 = self._conv2(x1)
         x3 = self._conv3(x2)
-        # print('x1', x1.size())
-        # print('x2', x2.size())
-        # print('x3', x3.size())
-        # print(short_cut.size())
         out = x3 + short_cut
         return out
 